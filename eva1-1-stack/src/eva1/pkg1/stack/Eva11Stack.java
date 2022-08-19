f48046e484d45923d84537b6b7c2adf79dbb95dc
Ame133 <65049181+ame133@users.noreply.github.com>
2022-08-19 09:48:12 -0600

    Update Eva11Stack.java

--- eva1-1-stack/src/eva1/pkg1/stack/Eva11Stack.java
+++ eva1-1-stack/src/eva1/pkg1/stack/Eva11Stack.java
@@ -15,17 +15,25 @@ public class Eva11Stack {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("INICIA MAIN");
         A();
+        System.out.println("RETOMA EJECUCION MAIN 1");
+        System.out.println("TERMINA MAIN");
+        
     }
     
     public static void A(){
+        System.out.println("INICIA A");
         B();
+        System.out.println("RETOMA EJECUCION A");
+        System.out.println("TERMINA A");
     }
     
     public static void B(){
+        System.out.println("INICIA B");
         int i = 1;
         double d = 10.0;
+        System.out.println("TERMINA B");
     }
     
 }
