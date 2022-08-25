6568d242a26042eb4b8894658690916b8ffed8a5
Ame133 <65049181+ame133@users.noreply.github.com>
2022-08-25 09:23:06 -0600

    Update Eva1_6_nanoTime.java

--- eva1_6_nanoTime/src/eva1_6_nanotime/Eva1_6_nanoTime.java
+++ eva1_6_nanoTime/src/eva1_6_nanotime/Eva1_6_nanoTime.java
@@ -3,20 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package eva1_6_nanotime;
+package eva_1_6_nanotime;
 
 /**
  *
  * @author moviles
  */
-public class Eva1_6_nanoTime {
+public class Eva_1_6_nanoTime {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println(System.nanoTime());
+        long ini =System.nanoTime();
+        System.out.println(ini);
+        long fin = System.nanoTime();
+        System.out.println(fin);
+        long tiempo = fin - ini;
+        System.out.println(tiempo);
     }
     
 }
